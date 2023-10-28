1d0a2703aaf4d4ceef31893f131a7449688276c6
dharmikpatel <dharmik529@gmail.com>
2023-10-27 20:21:14 -0400

    changes

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -45,8 +46,41 @@ void create_process(char *args[]){
   }
 }
 
+void listProcess() {
+  printf("List of running processes:\n");
+  system("ps -e");
+}
+
+void terminateProcess(int pid) {
+  int result = kill(pid, SIGTERM);
+  if (result == 0) {
+    printf("Process with PID %d terminted succesfully.\n", pid);
+  } else {
+    perror("Failed to terminate the process");
+  }
+}
+
+void monitorProcess(int pid) {
+  int status;
+  pid_t wait_pid;
+
+  wait_pid = waitpid(pid, &status, 0);
+
+  if (wait_pid == -1) {
+    perror("Wait for process failed");
+  } else {
+    if (WIFEXITED(status)) {
+      printf("Process with PID %d exited with status: %D\n", wait_pid, WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+      printf("Process with PID %d was terminated by signal: %d\n", wait_pid, WTERMSIG(status));
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
+
   char *args[argc];
+  /*
   printf("Arg Count: %d\n", argc);
   printf("Args: ");
   for (int i = 0; i < argc; ++i){
@@ -60,6 +94,54 @@ int main(int argc, char *argv[]) {
   args[argc-1] = NULL;
 
   create_process(args);
+*/
 
+  int choice, pid;
+
+  while(1) {
+    printf("\nSelect an option:\n");
+    printf("1. List processes\n");
+    printf("2. Terminate a process\n");
+    printf("3. Monitor a process\n");
+    printf("4. Create a process\n");
+    printf("4. Exit\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+      case 1:
+        listProcess();
+        break;
+      case 2:
+        printf("Enter the PID of the process to terminate: ");
+        scanf("%d", &pid);
+        terminateProcess(pid);
+        break;
+      case 3:
+        printf("Enter the PID of the process to monitor: ");
+        scanf("%d", &pid);
+        monitorProcess(pid);
+        break;
+      case 4:
+        printf("Arg Count: %d\n", argc);
+        printf("Args: ");
+        for (int i = 0; i < argc; ++i){
+          printf("%s ", argv[i]);
+          args[i] = argv[i];
+        }
+
+        for (int i = 1; i < argc; ++i){
+          args[i-1] = argv[i];
+        }
+        args[argc-1] = NULL;
+
+        create_process(args);
+      case 5:
+        exit(0);
+      default:
+        printf("Invalid choice. Please try again.\n");
+    }
+  }
   return 0;
 }
